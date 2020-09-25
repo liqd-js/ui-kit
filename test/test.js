c944a88a44ed83dcef29fb41ac1c053ccbe582c9
Tomáš Korenko <radixxko@gmail.com>
2020-09-25 14:47:07 +0200

    initial

---
+++ test/test.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const Server = require('@liqd-js/server');
+const Template = require('@liqd-js/template');
+const UIKit = require('../lib/ui_kit');
+
+const server = new Server();
+const template = new Template({ directories: [ UIKit.path, __dirname + '/templates' ]});
+
+server.get( '/form', async( req, res ) =>
+{
+    res.reply( await template.render( '', 'Form' ), 'text/html' );
+});
+
+server.get( async( req, res ) =>
+{
+    res.reply( await template.render( '', 'Index' ), 'text/html' );
+});
+
+server.listen( 8080 );
